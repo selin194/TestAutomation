9725c27c9de9112d0932c12fe6531a5cbfb29abb
selin194 <39696200+selin194@users.noreply.github.com>
2018-05-31 23:00:28 +0300

    Test1 is added.

--- Proje1/Test1_test.js
+++ Proje1/Test1_test.js
@@ -8,5 +8,15 @@ Scenario('test1', (I) => {
 	I.see('Ücretsiz* İlan Ver');
 	I.see('Detaylı Arama');
 	I.seeElement('input[id=searchText]');
+	//id
+	I.seeElement('#searchText');
+	//class
 	I.seeElement('.logo');
+	//xPath
+	I.seeNumberOfElements('//*[@class="uiBox showcase"]/ul/li',56);
+	
+	I.see('Vasıta');
+	I.see('Otomobil');
+	I.see('Arazi, SUV & Pick-up');
+	I.see('Motosiklet')
 });
