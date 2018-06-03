bc84cf0513135d11e27924dde5e7809d2b8f4983
selin194 <39696200+selin194@users.noreply.github.com>
2018-06-03 11:50:55 +0300

    Test3 updated.

--- Proje1/Test3_test.js
+++ Proje1/Test3_test.js
@@ -1,7 +1,7 @@
 Feature('Test3');
 
 let inputs = new DataTable(['name','value']); 
-inputs.add(['Yamaha MT07','62']);
+inputs.add(['Yamaha MT07','61']);
 inputs.add(['Yamaha MT09','25']);
 inputs.add(['Yamaha MT25','73']);
 
@@ -10,7 +10,7 @@ Data(inputs).Scenario('test3', async (I,current) => {
 	 I.fillField('query_text',current.name);
 	 I.executeScript(function() { document.getElementById('searchSuggestionForm').submit(); });
 	 I.click('Motosiklet');	
-	 let a = await I.grabTextFrom('//*[@id="searchResultsSearchForm"]/div/div[3]/div[1]/div[1]/div[1]/div[1]/span');
+	 let foundValue = await I.grabTextFrom('//*[@id="searchResultsSearchForm"]/div/div[3]/div[1]/div[1]/div[1]/div[1]/span');
 	 let assert =require('assert');
-	 assert.equal(current.value,a);	 
+	 assert.equal(current.value,foundValue);	 
 });
