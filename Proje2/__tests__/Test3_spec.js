daeb53eaa949a13e697b75195473f708fc3011ff
selin194 <39696200+selin194@users.noreply.github.com>
2018-06-09 13:52:05 +0300

    Proje2 Test3 is added.

---
+++ Proje2/__tests__/Test3_spec.js
@@ -0,0 +1,31 @@
+const frisby = require('C:/Users/User/AppData/Roaming/npm/node_modules/frisby');
+
+let readAlpha3_code;
+
+describe('Chaning', function () {
+  it('Test3', function () {
+    return frisby.get('http://services.groupkt.com/country/get/all')
+	.then(function (res){
+		var body = JSON.parse(res._body);
+	    restResponse = body["RestResponse"];
+		result = restResponse["result"];
+	
+		for (i = 0; i < result.length; i++) {
+			if(result[i].name == "India")
+			{
+				readAlpha3_code = result[i].alpha3_code;	
+				break;
+			}
+		}			
+	})
+      .then(function () { 
+       return frisby.get('http://services.groupkt.com/state/get/' + readAlpha3_code + '/all')
+        .expect('status', 200)
+        .expect('json', 'RestResponse.result.?', {
+			"id" : 65,
+			"largest_city" : "Srinagar Jammu",
+         })
+	});
+});
+});
+
