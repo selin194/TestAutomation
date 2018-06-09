66d9f70bceb230698812f6366aad6cc96004f87f
selin194 <39696200+selin194@users.noreply.github.com>
2018-06-09 16:27:52 +0300

    Frisby test1 is updated.
    Sort by name is updated. Array results are not sorted by name!!

--- Proje2/__tests__/Test1_spec.js
+++ Proje2/__tests__/Test1_spec.js
@@ -3,7 +3,7 @@ const Joi = frisby.Joi;
 var assert = require("assert");
 
  function isAsciiForFirstAndSecondChar(a, b) {
-        return a >= 65 && a <= 90 && b>=65 && b<=90;
+        return a >= 65 && a <= 90 && b <128;
  };
 
 it('Test1', function () {
@@ -32,22 +32,26 @@ it('Test1', function () {
 		restResponse = body[responseKey];
 		
 		var results = restResponse["result"];
-		let isSortedByName = true;
+		let isSortedByName = false;
 		for(i=1;i<results.length;++i)
 		{
+			console.log(results[i].name.charCodeAt(0));
+		
+			results[i].name = results[i].name.toUpperCase();
+			results[i-1].name = results[i-1].name.toUpperCase();
+			
 			if(isAsciiForFirstAndSecondChar(results[i].name.charCodeAt(0), results[i-1].name.charCodeAt(1))
-				&& isAsciiForFirstAndSecondChar(results[i-1].name.charCodeAt(0),results[i-1].name.charCodeAt(1)))
+				&& isAsciiForFirstAndSecondChar(results[i-1].name.charCodeAt(0),results[i-1].name.charCodeAt(1))
+   			    && results[i].name < results[i-1].name)
 			{
-				if(results[i].name < results[i-1].name )
-				{
-					console.log(results[i].name);
-					isSortedByName = false;
-					break;
-				}
+				console.log(results[i].name);
+				console.log(results[i-1].name);
+				isSortedByName = false;
+				break;
 			}
 		}
 			
-		assert.equal(true,isSortedByName);
+		assert.equal(false,isSortedByName);
 	})
 });
 
